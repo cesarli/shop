9e6c5ffcf462a520d9cf754755a91c2f73bc4d9a
cesarli <cesarli@163.com>
2015-04-21 01:15:54 +0800

    add html file

---
+++ Common/Conf/config.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'TMPL_PARSE_STRING'  =>array(
+        'TMPL_HOME_JS'     => '/Public/Home/js',
+        'TMPL_HOME_IMAGES'     => '/Public/Home/images',
+        'TMPL_HOME_CSS'     => '/Public/Home/css',
+    ),
+    'URL_HTML_SUFFIX'=>'',
+    'URL_MODEL'          => '3',
+    'MULTI_MODULE'          =>  false,
+    'DEFAULT_MODULE'        =>  'Home',
+
+);
---
+++ Home/Conf/config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'DEFAULT_CONTROLLER'    =>  'Index',
+    'DEFAULT_ACTION'        =>  'index',
+    'URL_ROUTER_ON'   => true,
+    'URL_ROUTE_RULES'=>array(
+        //'index/test'          => ''
+    ),
+);
