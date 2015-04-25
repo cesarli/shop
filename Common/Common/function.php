7316041461fca93be133f788021c54d2f9e98b01
cesarli <cesarli@163.com>
2015-04-25 16:40:51 +0800

    add

--- Common/Common/function.php
+++ Common/Common/function.php
@@ -4,3 +4,16 @@ function xmp($data){
     var_dump($data);
     echo "</pre>";
 }
+
+function getTree($arr, $p_id=0, $deep=0) {
+    static $tree = array();
+
+    foreach($arr as $row) {
+        if ($row['parent_id'] == $p_id) {
+            $row['deep'] = $deep;
+            $tree[] = $row;
+            getTree($arr, $row['class_id'], $deep+1);
+        }
+    }
+    return $tree;
+}
--- Manage/Conf/config.php
+++ Manage/Conf/config.php
@@ -3,5 +3,4 @@ return array(
 	//'配置项'=>'配置值'
     'URL_MODEL'          => '1',
     'DEFAULT_MODULE'     => 'Index',
-
 );
