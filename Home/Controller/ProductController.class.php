36ec732326524eec41e276b90bb6dc79d2dfec59
Faith Li <faith.li@toursforfun.com>
2015-04-30 13:38:11 +0800

    add submit

--- Home/Controller/ProductController.class.php
+++ Home/Controller/ProductController.class.php
@@ -4,8 +4,13 @@ use Think\Controller;
 class ProductController extends BaseController {
     public function plist(){
         $model = M('Goods');
-        $result = $model->where('is_trash = 0')->order('goods_sort desc')->select();
-        $this->assign('goods_info',$result);
+        $list = $model->where('is_trash = 0')->order('goods_sort desc')->page($_GET['p'].',15')->select();
+        $this->assign('goods_infso',$list);
+        $count      = $model->where('is_trash = 0')->count();//
+        $Page       = new \Think\Page($count,15);//
+        $Page->setConfig('header','个会员');
+        $show       = $Page->show();//
+        $this->assign('page',$show);//
         $this->display('plist');
     }
     public function instance(){
