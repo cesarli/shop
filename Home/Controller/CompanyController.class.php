f7e5a464f8e185b48f65f4d0b34b2428a404ba64
Faith Li <faith.li@toursforfun.com>
2015-04-30 16:33:17 +0800

    add some

--- Home/Controller/CompanyController.class.php
+++ Home/Controller/CompanyController.class.php
@@ -4,16 +4,18 @@ use Think\Controller;
 class CompanyController extends BaseController {
     public function news()
     {
-
+        $model = M('Article');
+        $result = $model->field('at_id,at_title,at_author,create_time,is_show')->order('at_id asc')->select();
+        $this->assign('article_info',$result);
         $this->display('news');
     }
 
     public function join()
     {
-        $this->display('join');
+        $this->display('join_form');
     }
     public function part()
     {
-        $this->display('part');
+        $this->display('about');
     }
 }
